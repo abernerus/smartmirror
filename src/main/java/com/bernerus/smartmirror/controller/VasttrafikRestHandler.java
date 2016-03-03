4be33e27c83a737d4017690e79789ce926fe5ace
andreas <andreas.bernerus@exertusit.se>
2016-03-03 22:04:10 +0100

    Cleanup rest handler

--- src/main/java/com/bernerus/smartmirror/controller/VasttrafikRestHandler.java
+++ src/main/java/com/bernerus/smartmirror/controller/VasttrafikRestHandler.java
@@ -1,9 +1,6 @@
 package com.bernerus.smartmirror.controller;
 
 import com.bernerus.smartmirror.api.VTTransportList;
-import com.bernerus.smartmirror.model.VasttrafikTokenStore;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,36 +13,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class VasttrafikRestHandler {
-  public static final String MUNKEBACKSMOTET_ID = "9021014004840000";
-  public static final String ATTEHOGSGATAN_ID = "9021014007750000";
-  public static final String HARLANDA_ID = "9021014003310000";
-  public static final String SVINGELN_ID = "9021014006480000";
-  private static final Logger log = LoggerFactory.getLogger(VasttrafikRestHandler.class);
-  private final VasttrafikTokenStore tokenStore = VasttrafikTokenStore.getInstance();
 
   @Autowired
   VasttrafikController vasttrafikController;
 
   @RequestMapping("/killtoken")
-  public
-  @ResponseBody
-  String killToken() {
+  public @ResponseBody String killToken() {
     return vasttrafikController.killToken();
   }
 
   @RequestMapping("/getid/{destinationName}")
-  public
-  @ResponseBody
-  String getId(
-    @PathVariable
-    String destinationName, Model model) {
+  public @ResponseBody
+  String getId(@PathVariable String destinationName, Model model) {
     return vasttrafikController.getId(destinationName, model);
   }
 
   @RequestMapping("/upcoming")
-  public
-  @ResponseBody
-  VTTransportList getUpcomingTransports() {
+  public @ResponseBody VTTransportList getUpcomingTransports() {
     return vasttrafikController.getUpcomingTransports();
   }
 
